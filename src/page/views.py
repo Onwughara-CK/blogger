f9250b255a44e62a03b5bcb4c448ab746c80e553
Collins <kelechicollins.93@gmail.com>
2020-08-01 15:14:38 +0100

    add HomeView, static files, url pattens

--- src/page/views.py
+++ src/page/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
+from . import models
+
+
+class HomeView(generic.ListView):
+    model = models.Post
+    template_name = "page/home.html"
+    ordering = ['-date_posted']
+    context_object_name = 'posts'
