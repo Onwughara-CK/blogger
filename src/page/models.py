367ffde57060aaf1d6be802efed7c4e59349cd0b
Collins <kelechicollins.93@gmail.com>
2020-08-01 14:09:41 +0100

    add page app, post model, added post to admin

---
+++ src/page/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField('content')
+    date_posted = models.DateTimeField('time published', default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
