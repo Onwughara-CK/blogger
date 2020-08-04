518abe6e58a17e4dafd7506293106571b862ba59
Collins <kelechicollins.93@gmail.com>
2020-08-04 09:26:37 +0100

    overwrite save function of Profile model to resize uploaded images

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -12,3 +13,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}'
+
+    def save(self):
+        super().save()
+
+        image_path = self.image.path
+        image = Image.open(image_path)
+        MAX_SIZE = (125, 125)
+        image.thumbnail(MAX_SIZE)
+        image.save()
