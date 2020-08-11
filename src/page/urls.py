7a5e7a3ea814f0233d3365f0beb61bf6647cb2fa
Collins <kelechicollins.93@gmail.com>
2020-08-11 11:05:13 +0100

    add backslash in urlconf

--- src/page/urls.py
+++ src/page/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
          name='redirect-to-detail'),
     path('post/<int:pk>/<str:slug>', views.PostDetailView.as_view(), name='detail'),
     path('post/create', views.PostCreateView.as_view(), name='create'),
-    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='update'),
-    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='delete'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete'),
     # path('post/<str:username>', views.userPostListView, name='user-posts'),
     path('post/<str:username>', views.UserPostListView.as_view(), name='user-posts')
 ]
